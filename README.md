0bc5b2ce637a904cabbd3a216392dfd314d7a4fd
larissasouto <larissa@redes.ufsm.br>
2021-10-28 16:05:03 -0300

    gpiozero library

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # lamp-loads-Modbus
+
+## Dependencies
+
+1. Install PyModbus
+
+        pip3 install pymodbus
+
+2. Install gpiozero library
+
+        sudo apt install python3-gpiozero
--- lamp_loads.py
+++ lamp_loads.py
@@ -1,5 +1,5 @@
 from pymodbus.client.sync import ModbusTcpClient
-import RPi.GPIO as GPIO
+import gpiozero
 import time
 
 # server connection
@@ -13,82 +13,82 @@ print("[+]Info : Connection : " + str(client.connect()))
 UNIT = 2
 # server connection
 
-# Rpi pin definition
-relay1 = 29
-relay2 = 31
-relay3 = 33
-relay4 = 36
-relay5 = 35
-relay6 = 38
-relay7 = 40
+""" 
+Rpi pin definition
 
-# Pin Setup
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
+PIN | GPIO
+29     5
+31     6
+33     13
+36     16
+35     19
+38     20
+40     21
+"""
 
-GPIO.setup(relay1, GPIO.OUT)
-GPIO.setup(relay2, GPIO.OUT)
-GPIO.setup(relay3, GPIO.OUT)
-GPIO.setup(relay4, GPIO.OUT)
-GPIO.setup(relay5, GPIO.OUT)
-GPIO.setup(relay6, GPIO.OUT)
-GPIO.setup(relay7, GPIO.OUT)
+relay1 = gpiozero.OutputDevice(5, initial_value=True)
+relay2 = gpiozero.OutputDevice(6, initial_value=True)
+relay3 = gpiozero.OutputDevice(13, initial_value=True)
+relay4 = gpiozero.OutputDevice(16, initial_value=True)
+relay5 = gpiozero.OutputDevice(19, initial_value=True)
+relay6 = gpiozero.OutputDevice(20, initial_value=True)
+relay7 = gpiozero.OutputDevice(21, initial_value=True)
 
 # initial states Load 1
-prevRTAC_C1 = 1
-nextRTAC_C1 = 1
-prevManual_S1 = 1
-nextManual_S1 = 1
-prevFlipFlop_L1 = 1
-nextFlipFlop_L1 = 1
+prevRTAC_C1 = 0
+nextRTAC_C1 = 0
+prevManual_S1 = 0
+nextManual_S1 = 0
+prevFlipFlop_L1 = 0
+nextFlipFlop_L1 = 0
 
 # initial states Load 2
-prevRTAC_C2 = 1
-nextRTAC_C2 = 1
-prevManual_S2 = 1
-nextManual_S2 = 1
-prevFlipFlop_L2 = 1
-nextFlipFlop_L2 = 1
+prevRTAC_C2 = 0
+nextRTAC_C2 = 0
+prevManual_S2 = 0
+nextManual_S2 = 0
+prevFlipFlop_L2 = 0
+nextFlipFlop_L2 = 0
 
 # initial states Load 3
-prevRTAC_C3 = 1
-nextRTAC_C3 = 1
-prevManual_S3 = 1
-nextManual_S3 = 1
-prevFlipFlop_L3 = 1
-nextFlipFlop_L3 = 1
+prevRTAC_C3 = 0
+nextRTAC_C3 = 0
+prevManual_S3 = 0
+nextManual_S3 = 0
+prevFlipFlop_L3 = 0
+nextFlipFlop_L3 = 0
 
 # initial states Load 4
-prevRTAC_C4 = 1
-nextRTAC_C4 = 1
-prevManual_S4 = 1
-nextManual_S4 = 1
-prevFlipFlop_L4 = 1
-nextFlipFlop_L4 = 1
+prevRTAC_C4 = 0
+nextRTAC_C4 = 0
+prevManual_S4 = 0
+nextManual_S4 = 0
+prevFlipFlop_L4 = 0
+nextFlipFlop_L4 = 0
 
 # initial states Load 5
-prevRTAC_C5 = 1
-nextRTAC_C5 = 1
-prevManual_S5 = 1
-nextManual_S5 = 1
-prevFlipFlop_L5 = 1
-nextFlipFlop_L5 = 1
+prevRTAC_C5 = 0
+nextRTAC_C5 = 0
+prevManual_S5 = 0
+nextManual_S5 = 0
+prevFlipFlop_L5 = 0
+nextFlipFlop_L5 = 0
 
 # initial states Load 6
-prevRTAC_C6 = 1
-nextRTAC_C6 = 1
-prevManual_S6 = 1
-nextManual_S6 = 1
-prevFlipFlop_L6 = 1
-nextFlipFlop_L6 = 1
+prevRTAC_C6 = 0
+nextRTAC_C6 = 0
+prevManual_S6 = 0
+nextManual_S6 = 0
+prevFlipFlop_L6 = 0
+nextFlipFlop_L6 = 0
 
 # initial states Load 7
-prevRTAC_C7 = 1
-nextRTAC_C7 = 1
-prevManual_S7 = 1
-nextManual_S7 = 1
-prevFlipFlop_L7 = 1
-nextFlipFlop_L7 = 1
+prevRTAC_C7 = 0
+nextRTAC_C7 = 0
+prevManual_S7 = 0
+nextManual_S7 = 0
+prevFlipFlop_L7 = 0
+nextFlipFlop_L7 = 0
 
 
 # LOGIC FUNCTIONS
@@ -101,106 +101,105 @@ def pset_mode(value):
 
     if pset_power < 828:
         print("S1 OFF\nS2 OFF\nS3 OFF\nS4 OFF\nS5 OFF\nS6 OFF\nS7 OFF\n")
-        GPIO.output(relay1, 1)
-        GPIO.output(relay2, 1)
-        GPIO.output(relay3, 1)
-        GPIO.output(relay4, 1)
-        GPIO.output(relay5, 1)
-        GPIO.output(relay6, 1)
-        GPIO.output(relay7, 1)
+        relay1.on() # inverted logic
+        relay2.on()
+        relay3.on()
+        relay4.on()
+        relay5.on()
+        relay6.on()
+        relay7.on()
 
-        client.write_coils(9, [1] * 7, unit=UNIT)  # RTAC loads state (OFF)
+        client.write_coils(9, [1] * 7, unit=UNIT)  # RTAC loads state (OFF = 1)
 
     if (pset_power >= 828) and (pset_power < 1656):
         print("S1 ON\nS2 OFF\nS3 OFF\nS4 OFF\nS5 OFF\nS6 OFF\nS7 OFF\n")
-        GPIO.output(relay1, 0)
-        GPIO.output(relay2, 1)
-        GPIO.output(relay3, 1)
-        GPIO.output(relay4, 1)
-        GPIO.output(relay5, 1)
-        GPIO.output(relay6, 1)
-        GPIO.output(relay7, 1)
+        relay1.off()
+        relay2.on()
+        relay3.on()
+        relay4.on()
+        relay5.on()
+        relay6.on()
+        relay7.on()
 
-        client.write_coil(9, 0, unit=UNIT)  # RTAC loads state (ON)
-        client.write_coils(10, [1] * 6, unit=UNIT)  # RTAC loads state (OFF)
+        client.write_coil(9, 0, unit=UNIT)  # RTAC loads state (ON = 0)
+        client.write_coils(10, [1] * 6, unit=UNIT)  # RTAC loads state (OFF = 1)
 
     if (pset_power >= 1656) and (pset_power < 2484):
         print("S1 ON\nS2 ON\nS3 OFF\nS4 OFF\nS5 OFF\nS6 OFF\nS7 OFF\n")
-        GPIO.output(relay1, 0)
-        GPIO.output(relay2, 0)
-        GPIO.output(relay3, 1)
-        GPIO.output(relay4, 1)
-        GPIO.output(relay5, 1)
-        GPIO.output(relay6, 1)
-        GPIO.output(relay7, 1)
+        relay1.off()
+        relay2.off()
+        relay3.on()
+        relay4.on()
+        relay5.on()
+        relay6.on()
+        relay7.on()
 
-        client.write_coils(9, [0] * 2, unit=UNIT)  # RTAC loads state (ON)
-        client.write_coils(11, [1] * 5, unit=UNIT)  # RTAC loads state (OFF)
+        client.write_coils(9, [0] * 2, unit=UNIT)  # RTAC loads state (ON = 0)
+        client.write_coils(11, [1] * 5, unit=UNIT)  # RTAC loads state (OFF = 1)
 
     if (pset_power >= 2484) and (pset_power < 3312):
         print("S1 ON\nS2 ON\nS3 ON\nS4 OFF\nS5 OFF\nS6 OFF\nS7 OFF\n")
-        GPIO.output(relay1, 0)
-        GPIO.output(relay2, 0)
-        GPIO.output(relay3, 0)
-        GPIO.output(relay4, 1)
-        GPIO.output(relay5, 1)
-        GPIO.output(relay6, 1)
-        GPIO.output(relay7, 1)
+        relay1.off()
+        relay2.off()
+        relay3.off()
+        relay4.on()
+        relay5.on()
+        relay6.on()
+        relay7.on()
 
-        client.write_coils(9, [0] * 3, unit=UNIT)  # RTAC loads state (ON)
-        client.write_coils(12, [1] * 4, unit=UNIT)  # RTAC loads state (OFF)
+        client.write_coils(9, [0] * 3, unit=UNIT)  # RTAC loads state (ON = 0)
+        client.write_coils(12, [1] * 4, unit=UNIT)  # RTAC loads state (OFF = 1)
 
     if (pset_power >= 3312) and (pset_power < 4140):
         print("S1 ON\nS2 ON\nS3 ON\nS4 ON\nS5 OFF\nS6 OFF\nS7 OFF\n")
-        GPIO.output(relay1, 0)
-        GPIO.output(relay2, 0)
-        GPIO.output(relay3, 0)
-        GPIO.output(relay4, 0)
-        GPIO.output(relay5, 1)
-        GPIO.output(relay6, 1)
-        GPIO.output(relay7, 1)
+        relay1.off()
+        relay2.off()
+        relay3.off()
+        relay4.off()
+        relay5.on()
+        relay6.on()
+        relay7.on()
 
-        client.write_coils(9, [0] * 4, unit=UNIT)  # RTAC loads state (ON)
-        client.write_coils(13, [1] * 3, unit=UNIT)  # RTAC loads state (OFF)
+        client.write_coils(9, [0] * 4, unit=UNIT)  # RTAC loads state (ON = 0)
+        client.write_coils(13, [1] * 3, unit=UNIT)  # RTAC loads state (OFF = 1)
 
     if (pset_power >= 4140) and (pset_power < 4968):
         print("S1 ON\nS2 ON\nS3 ON\nS4 ON\nS5 ON\nS6 OFF\nS7 OFF\n")
-        GPIO.output(relay1, 0)
-        GPIO.output(relay2, 0)
-        GPIO.output(relay3, 0)
-        GPIO.output(relay4, 0)
-        GPIO.output(relay5, 0)
-        GPIO.output(relay6, 1)
-        GPIO.output(relay7, 1)
+        relay1.off()
+        relay2.off()
+        relay3.off()
+        relay4.off()
+        relay5.off()
+        relay6.on()
+        relay7.on()
 
-        client.write_coils(9, [0] * 5, unit=UNIT)  # RTAC loads state (ON)
-        client.write_coils(14, [1] * 2, unit=UNIT)  # RTAC loads state (OFF)
+        client.write_coils(9, [0] * 5, unit=UNIT)  # RTAC loads state (ON = 0)
+        client.write_coils(14, [1] * 2, unit=UNIT)  # RTAC loads state (OFF = 1)
 
     if (pset_power >= 4968) and (pset_power < 5796):
         print("S1 ON\nS2 ON\nS3 ON\nS4 ON\nS5 ON\nS6 ON\nS7 OFF\n")
-        GPIO.output(relay1, 0)
-        GPIO.output(relay2, 0)
-        GPIO.output(relay3, 0)
-        GPIO.output(relay4, 0)
-        GPIO.output(relay5, 0)
-        GPIO.output(relay6, 0)
-        GPIO.output(relay7, 1)
+        relay1.off()
+        relay2.off()
+        relay3.off()
+        relay4.off()
+        relay5.off()
+        relay6.off()
+        relay7.on()
 
-        client.write_coils(9, [0] * 6, unit=UNIT)  # RTAC loads state (ON)
-        client.write_coil(15, 1, unit=UNIT)  # RTAC loads state (OFF)
+        client.write_coils(9, [0] * 6, unit=UNIT)  # RTAC loads state (ON = 0)
+        client.write_coil(15, 1, unit=UNIT)  # RTAC loads state (OFF = 1)
 
     if pset_power >= 5796:
         print("S1 ON\nS2 ON\nS3 ON\nS4 ON\nS5 ON\nS6 ON\nS7 ON\n")
-        GPIO.output(relay1, 0)
-        GPIO.output(relay2, 0)
-        GPIO.output(relay3, 0)
-        GPIO.output(relay4, 0)
-        GPIO.output(relay5, 0)
-        GPIO.output(relay6, 0)
-        GPIO.output(relay7, 0)
+        relay1.off()
+        relay2.off()
+        relay3.off()
+        relay4.off()
+        relay5.off()
+        relay6.off()
+        relay7.off()
 
-        client.write_coils(9, [0] * 7, unit=UNIT)  # RTAC loads state (ON)
-        state = GPIO.input(relay7)
+        client.write_coils(9, [0] * 7, unit=UNIT)  # RTAC loads state (ON = 0)
 
 
 def risingEdgeDetector(prevState, nextState):
@@ -242,25 +241,25 @@ def orLogic(RTAC, manual):
 
 def rtac_mode(values, load):
     # initial states Load 1
-    global prevRTAC_C1, nextRTAC_C1, prevManual_S1, nextManual_S1, prevFlipFlop_L1, prevFlipFlop_L1
+    global prevRTAC_C1, nextRTAC_C1, prevManual_S1, nextManual_S1
 
     # initial states Load 2
-    global prevRTAC_C2, nextRTAC_C2, prevManual_S2, nextManual_S2, prevFlipFlop_L2, prevFlipFlop_L2
+    global prevRTAC_C2, nextRTAC_C2, prevManual_S2, nextManual_S2
 
     # initial states Load 3
-    global prevRTAC_C3, nextRTAC_C3, prevManual_S3, nextManual_S3, prevFlipFlop_L3, prevFlipFlop_L3
+    global prevRTAC_C3, nextRTAC_C3, prevManual_S3, nextManual_S3
 
     # initial states Load 4
-    global prevRTAC_C4, nextRTAC_C4, prevManual_S4, nextManual_S4, prevFlipFlop_L4, prevFlipFlop_L4
+    global prevRTAC_C4, nextRTAC_C4, prevManual_S4, nextManual_S4
 
     # initial states Load 5
-    global prevRTAC_C5, nextRTAC_C5, prevManual_S5, nextManual_S5, prevFlipFlop_L5, prevFlipFlop_L5
+    global prevRTAC_C5, nextRTAC_C5, prevManual_S5, nextManual_S5
 
     # initial states Load 6
-    global prevRTAC_C6, nextRTAC_C6, prevManual_S6, nextManual_S6, prevFlipFlop_L6, prevFlipFlop_L6
+    global prevRTAC_C6, nextRTAC_C6, prevManual_S6, nextManual_S6
 
     # initial states Load 7
-    global prevRTAC_C7, nextRTAC_C7, prevManual_S7, nextManual_S7, prevFlipFlop_L7, prevFlipFlop_L7
+    global prevRTAC_C7, nextRTAC_C7, prevManual_S7, nextManual_S7
 
     if load == 1:
         prevRTAC_C1 = nextRTAC_C1
@@ -268,9 +267,8 @@ def rtac_mode(values, load):
         nextRTAC_C1 = response_RTAC
 
         prevManual_S1 = nextManual_S1
-        response_Manual = client.read_holding_registers(1, 1, unit=UNIT)  # HR 1 - C1 cmd (simulation)
-#        nextManual_S1 = response_Manual.registers[0]
-        nextManual_S1 = GPIO.input(relay1)
+        nextManual_S1 = not relay1.value
+        
 
         risingEdgeRTAC = risingEdgeDetector(prevRTAC_C1, nextRTAC_C1)
         risingEdgeManual = risingEdgeDetector(prevManual_S1, nextManual_S1)
@@ -288,8 +286,13 @@ def rtac_mode(values, load):
         loadState = srFlipFlop(s, r, prevFlipFlop_L1, nextFlipFlop_L1)
         nextFlipFlop_L1 = loadState
 
-        GPIO.output(relay1, loadState)
+        if(loadState == 0):
+            relay1.on() # load OFF
+        else:
+            relay1.off() # load ON
+
         client.write_coil(9, loadState, unit=UNIT)
+        print ("PIN 29: ", relay1.value)
         return loadState
 
     if load == 2:
@@ -298,9 +301,7 @@ def rtac_mode(values, load):
         nextRTAC_C2 = response_RTAC
 
         prevManual_S2 = nextManual_S2
-        response_Manual = client.read_holding_registers(2, 1, unit=UNIT)  # HR 2 - C2 cmd (simulation)
-#        nextManual_S2 = response_Manual.registers[0]
-        nextManual_S2 = GPIO.input(relay2)
+        nextManual_S2 = not relay2.value
 
         risingEdgeRTAC = risingEdgeDetector(prevRTAC_C2, nextRTAC_C2)
         risingEdgeManual = risingEdgeDetector(prevManual_S2, nextManual_S2)
@@ -318,8 +319,13 @@ def rtac_mode(values, load):
         loadState = srFlipFlop(s, r, prevFlipFlop_L2, nextFlipFlop_L2)
         nextFlipFlop_L2 = loadState
 
-        GPIO.output(relay2, loadState)
+        if(loadState == 0):
+            relay2.on() # load OFF
+        else:
+            relay2.off() # load ON
+
         client.write_coil(10, loadState, unit=UNIT)
+        print ("PIN 31: ", relay2.value)
         return loadState
 
     if load == 3:
@@ -328,9 +334,7 @@ def rtac_mode(values, load):
         nextRTAC_C3 = response_RTAC
 
         prevManual_S3 = nextManual_S3
-        response_Manual = client.read_holding_registers(3, 1, unit=UNIT)  # HR 3 - C3 cmd (simulation)
-#        nextManual_S3 = response_Manual.registers[0]
-        nextManual_S3 = GPIO.input(relay3)
+        nextManual_S3 = not relay3.value
 
         risingEdgeRTAC = risingEdgeDetector(prevRTAC_C3, nextRTAC_C3)
         risingEdgeManual = risingEdgeDetector(prevManual_S3, nextManual_S3)
@@ -348,8 +352,13 @@ def rtac_mode(values, load):
         loadState = srFlipFlop(s, r, prevFlipFlop_L3, nextFlipFlop_L3)
         nextFlipFlop_L3 = loadState
 
-        GPIO.output(relay3, loadState)
+        if(loadState == 0):
+            relay3.on() # load OFF
+        else:
+            relay3.off() # load ON
+
         client.write_coil(11, loadState, unit=UNIT)
+        print ("PIN 33: ", relay3.value)
         return loadState
 
     if load == 4:
@@ -358,9 +367,7 @@ def rtac_mode(values, load):
         nextRTAC_C4 = response_RTAC
 
         prevManual_S4 = nextManual_S4
-        response_Manual = client.read_holding_registers(4, 1, unit=UNIT)  # HR 4 - C4 cmd (simulation)
-#        nextManual_S4 = response_Manual.registers[0]
-        nextManual_S4 = GPIO.input(relay4)
+        nextManual_S4 = not relay4.value
 
         risingEdgeRTAC = risingEdgeDetector(prevRTAC_C4, nextRTAC_C4)
         risingEdgeManual = risingEdgeDetector(prevManual_S4, nextManual_S4)
@@ -378,8 +385,13 @@ def rtac_mode(values, load):
         loadState = srFlipFlop(s, r, prevFlipFlop_L4, nextFlipFlop_L4)
         nextFlipFlop_L4 = loadState
 
-        GPIO.output(relay4, loadState)
+        if(loadState == 0):
+            relay4.on() # load OFF
+        else:
+            relay4.off() # load ON
+
         client.write_coil(12, loadState, unit=UNIT)
+        print ("PIN 36: ", relay4.value)
         return loadState
 
     if load == 5:
@@ -388,9 +400,7 @@ def rtac_mode(values, load):
         nextRTAC_C5 = response_RTAC
 
         prevManual_S5 = nextManual_S5
-        response_Manual = client.read_holding_registers(5, 1, unit=UNIT)  # HR 5 - C5 cmd (simulation)
-#        nextManual_S5 = response_Manual.registers[0]
-        nextManual_S5 = GPIO.input(relay5)
+        nextManual_S5 = not relay5.value
 
         risingEdgeRTAC = risingEdgeDetector(prevRTAC_C5, nextRTAC_C5)
         risingEdgeManual = risingEdgeDetector(prevManual_S5, nextManual_S5)
@@ -408,8 +418,13 @@ def rtac_mode(values, load):
         loadState = srFlipFlop(s, r, prevFlipFlop_L5, nextFlipFlop_L5)
         nextFlipFlop_L5 = loadState
 
-        GPIO.output(relay5, loadState)
+        if(loadState == 0):
+            relay5.on() # load OFF
+        else:
+            relay5.off() # load ON
+
         client.write_coil(13, loadState, unit=UNIT)
+        print ("PIN 35: ", relay5.value)
         return loadState
 
     if load == 6:
@@ -418,9 +433,7 @@ def rtac_mode(values, load):
         nextRTAC_C6 = response_RTAC
 
         prevManual_S6 = nextManual_S6
-        response_Manual = client.read_holding_registers(6, 1, unit=UNIT)  # HR 6 - C6 cmd (simulation)
-#        nextManual_S6 = response_Manual.registers[0]
-        nextManual_S6 = GPIO.input(relay6)
+        nextManual_S6 = not relay6.value
 
         risingEdgeRTAC = risingEdgeDetector(prevRTAC_C6, nextRTAC_C6)
         risingEdgeManual = risingEdgeDetector(prevManual_S6, nextManual_S6)
@@ -438,8 +451,13 @@ def rtac_mode(values, load):
         loadState = srFlipFlop(s, r, prevFlipFlop_L6, nextFlipFlop_L6)
         nextFlipFlop_L6 = loadState
 
-        GPIO.output(relay6, loadState)
+        if(loadState == 0):
+            relay6.on() # load OFF
+        else:
+            relay6.off() # load ON
+
         client.write_coil(14, loadState, unit=UNIT)
+        print ("PIN 38: ", relay6.value)
         return loadState
 
     if load == 7:
@@ -448,9 +466,7 @@ def rtac_mode(values, load):
         nextRTAC_C7 = response_RTAC
 
         prevManual_S7 = nextManual_S7
-        response_Manual = client.read_holding_registers(7, 1, unit=UNIT)  # HR 7 - C7 cmd (simulation)
-#        nextManual_S7 = response_Manual.registers[0]
-        nextManual_S7 = GPIO.input(relay7)
+        nextManual_S7 = not relay7.value
 
         risingEdgeRTAC = risingEdgeDetector(prevRTAC_C7, nextRTAC_C7)
         risingEdgeManual = risingEdgeDetector(prevManual_S7, nextManual_S7)
@@ -468,9 +484,13 @@ def rtac_mode(values, load):
         loadState = srFlipFlop(s, r, prevFlipFlop_L7, nextFlipFlop_L7)
         nextFlipFlop_L7 = loadState
 
-        GPIO.output(relay7, loadState)
+        if(loadState == 0):
+            relay7.on() # load OFF
+        else:
+            relay7.off() # load ON
+
         client.write_coil(15, loadState, unit=UNIT)
-        state = GPIO.input(relay7)
+        print ("PIN 40: ", relay7.value)
         return loadState
 
 
@@ -483,25 +503,46 @@ def getValues():
         values = client.read_coils(2, 9, unit=UNIT)
         #  Load 1
         loadState = rtac_mode(values, 1)
-        print("SWITCH 1: {I}".format(I=loadState))
+        if loadState == 0:
+            print("SWITCH 1: OFF")
+        else:
+            print("SWITCH 1 = ON")
         #  Load 2
         loadState = rtac_mode(values, 2)
-        print("SWITCH 2: {I}".format(I=loadState))
+        if loadState == 0:
+            print("SWITCH 2: OFF")
+        else:
+            print("SWITCH 2 = ON")
         #  Load 3
         loadState = rtac_mode(values, 3)
-        print("SWITCH 3: {I}".format(I=loadState))
+        if loadState == 0:
+            print("SWITCH 3: OFF")
+        else:
+            print("SWITCH 3 = ON")
         #  Load 4
         loadState = rtac_mode(values, 4)
-        print("SWITCH 4: {I}".format(I=loadState))
+        if loadState == 0:
+            print("SWITCH 4: OFF")
+        else:
+            print("SWITCH 4 = ON")
         #  Load 5
         loadState = rtac_mode(values, 5)
-        print("SWITCH 5: {I}".format(I=loadState))
+        if loadState == 0:
+            print("SWITCH 5: OFF")
+        else:
+            print("SWITCH 5 = ON")
         #  Load 6
         loadState = rtac_mode(values, 6)
-        print("SWITCH 6: {I}".format(I=loadState))
+        if loadState == 0:
+            print("SWITCH 6: OFF")
+        else:
+            print("SWITCH 6 = ON")
         #  Load 7
         loadState = rtac_mode(values, 7)
-        print("SWITCH 7: {I}\n".format(I=loadState))
+        if loadState == 0:
+            print("SWITCH 7: OFF")
+        else:
+            print("SWITCH 7 : ON")
 
     else:
         power_value = client.read_holding_registers(0, 1, unit=UNIT)
